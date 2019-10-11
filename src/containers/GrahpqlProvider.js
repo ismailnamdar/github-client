a93d245c7048aaa2194bfc1e0e98eae38bab5de5
İsmail Namdar <namdari@itu.edu.tr>
2019-10-12 01:50:40 +0300

    feat(GraphqlProvider): add comp, authentication

--- src/containers/GrahpqlProvider.js
+++ src/containers/GrahpqlProvider.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import {GRAPHQL_API_BASE_URL} from "../configs/constants";
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: GRAPHQL_API_BASE_URL
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token') || process.env.REACT_APP_AUTH_TOKEN;
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  // uri: GRAPHQL_API_BASE_URL,
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
 });
 
 const GraphqlProvider = ({ children }) => (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const READ_REPO = gql`
+   query {
+    repository(name:"reactjs") {
+      issues(last:20, states:CLOSED) {
+        edges {
+          node {
+            title
+            url
+            labels(first:5) {
+              edges {
+                node {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 const Home = () => {
-  return <div>Selam</div>
+  const {data = {}} = useQuery(READ_REPO);
+  return <div>{JSON.stringify(data, 0, 2)}</div>
 };
 
 export default Home;
